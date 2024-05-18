99ad8d88ecb62c5ca5c262748767c0e0fb3f1a31
MaxUlianov <curze.curze22@gmail.com>
2024-05-18 15:18:41 +0200

    Init project

---
+++ database.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "database/sql"
+  "fmt"
+  "log"
+  "os"
+  
+  "github.com/lib/pq"
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module lotw_after_hours/data_policy_tracker
+
+go 1.22.0
+
+require github.com/lib/pq v1.10.9
