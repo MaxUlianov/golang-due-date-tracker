808c07afe1ee83b983e422d3f1c00c57af27f9fe
MaxUlianov <curze.curze22@gmail.com>
2024-07-09 22:13:21 +0200

    Added Postgres handling file

---
+++ data_policy_tracker.go
@@ -0,0 +1,9 @@
+package data_policy_tracker
+
+import "fmt"
+
+func main() {
+	handlePostgres()
+
+	fmt.Println("main file")
+}
--- database.go
+++ database.go
@@ -1,10 +1,53 @@
-package main
+package data_policy_tracker
 
 import (
-  "database/sql"
-  "fmt"
-  "log"
-  "os"
-  
-  "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
+
+type DBConfig struct {
+	User     string
+	Password string
+	Host     string
+	Port     string
+	Name     string
+}
+
+func NewDBConfig() *DBConfig {
+	return &DBConfig{
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+}
+
+func handlePostgres() {
+	postgresConfig := NewDBConfig()
+
+	connectString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		postgresConfig.User,
+		postgresConfig.Password, postgresConfig.Host, postgresConfig.Port, postgresConfig.Name)
+
+	var err error
+	db, err = sql.Open("postgres", connectString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// check connect
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
